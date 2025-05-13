f6fed8ee803705ed4d60ec8c21538fa0d53d738e
{your_name} <{your_email}>
2025-05-14 05:25:45 +0600

    changes on

--- src/components/tennis/TennisAcademySection.jsx
+++ src/components/tennis/TennisAcademySection.jsx
@@ -49,7 +49,8 @@ const TennisAcademySection = () => {
       icon: <Clock className="w-12 h-12 text-yellow-500" />,
       title: "Tennis Court Rent",
       description: "Book high-quality tennis courts for practice, matches, or personal training.",
-      image:{COART}
+     
+      image: "https://i.pinimg.com/736x/70/d8/85/70d8851d82dbdf112314f58ff213828a.jpg"
     }
   ];
 
