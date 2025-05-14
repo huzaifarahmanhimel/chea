afc998231b6a9a846a19c2e965af9f231c728afb
{your_name} <{your_email}>
2025-05-15 04:10:46 +0600

    changes

--- src/components/home/CatagorySections.jsx
+++ src/components/home/CatagorySections.jsx
@@ -75,8 +75,8 @@ export default function CategorySections() {
       </div>
       
       {/* Treatment Cards with Advanced Hover Effects */}
-      <div className="px-80 pb-40">
-        <h2 className="text-center text-3xl md:text-4xl font-light tracking-wider mb-16 text-green-600">
+      <div className="px-5 pb-6">
+        <h2 className="text-center text-3xl md:text-4xl font-light tracking-wider mb-8 text-green-600">
           EXPERIENCE <span className="font-bold"> ALOFTY LIFE</span>
         </h2>
         
